51e597db61f876b807f946a9a47198d184b9f788
gganley <gganley@student.bridgew.edu>
2018-12-11 11:40:20 -0500

    Change to table.go in examples

--- stoverflow.go
+++ stoverflow.go
@@ -1,196 +1,51 @@
-// 12 august 2018
-
 package main
 
 import (
-	"github.com/andlabs/ui"
 	"fmt"
+	"github.com/andlabs/ui"
+	"strconv"
 )
 
-var mainwin *ui.Window
-
-func makeBasicControlsPage() ui.Control {
-	vbox := ui.NewVerticalBox()
-	vbox.SetPadded(true)
-
-	hbox := ui.NewHorizontalBox()
-	hbox.SetPadded(true)
-	vbox.Append(hbox, false)
-
-	hbox.Append(ui.NewButton("Button"), false)
-	hbox.Append(ui.NewCheckbox("Checkbox"), false)
-
-	vbox.Append(ui.NewLabel("This is a label. Right now, labels can only span one line."), false)
-
-	vbox.Append(ui.NewHorizontalSeparator(), false)
-
-	group := ui.NewGroup("Entries")
-	group.SetMargined(true)
-	vbox.Append(group, true)
-
-	group.SetChild(ui.NewNonWrappingMultilineEntry())
-
-	entryForm := ui.NewForm()
-	entryForm.SetPadded(true)
-	group.SetChild(entryForm)
-
-	entryForm.Append("Entry", ui.NewEntry(), false)
-	entryForm.Append("Password Entry", ui.NewPasswordEntry(), false)
-	entryForm.Append("Search Entry", ui.NewSearchEntry(), false)
-	entryForm.Append("Multiline Entry", ui.NewMultilineEntry(), true)
-	entryForm.Append("Multiline Entry No Wrap", ui.NewNonWrappingMultilineEntry(), true)
-
-	return vbox
+type modelHandler struct {
 }
 
-func makeNumbersPage() ui.Control {
-	hbox := ui.NewHorizontalBox()
-	hbox.SetPadded(true)
-
-	group := ui.NewGroup("Numbers")
-	group.SetMargined(true)
-	hbox.Append(group, true)
-
-	vbox := ui.NewVerticalBox()
-	vbox.SetPadded(true)
-	group.SetChild(vbox)
-
-	spinbox := ui.NewSpinbox(0, 100)
-	slider := ui.NewSlider(0, 100)
-	pbar := ui.NewProgressBar()
-	spinbox.OnChanged(func(*ui.Spinbox) {
-		slider.SetValue(spinbox.Value())
-		pbar.SetValue(spinbox.Value())
-	})
-	slider.OnChanged(func(*ui.Slider) {
-		spinbox.SetValue(slider.Value())
-		pbar.SetValue(slider.Value())
-	})
-	vbox.Append(spinbox, false)
-	vbox.Append(slider, false)
-	vbox.Append(pbar, false)
-
-	ip := ui.NewProgressBar()
-	ip.SetValue(-1)
-	vbox.Append(ip, false)
-
-	group = ui.NewGroup("Lists")
-	group.SetMargined(true)
-	hbox.Append(group, true)
-
-	vbox = ui.NewVerticalBox()
-	vbox.SetPadded(true)
-	group.SetChild(vbox)
-
-	cbox := ui.NewCombobox()
-	cbox.Append("Combobox Item 1")
-	cbox.Append("Combobox Item 2")
-	cbox.Append("Combobox Item 3")
-	vbox.Append(cbox, false)
-
-	ecbox := ui.NewEditableCombobox()
-	ecbox.Append("Editable Item 1")
-	ecbox.Append("Editable Item 2")
-	ecbox.Append("Editable Item 3")
-	vbox.Append(ecbox, false)
-
-	rb := ui.NewRadioButtons()
-	rb.Append("Radio Button 1")
-	rb.Append("Radio Button 2")
-	rb.Append("Radio Button 3")
-	vbox.Append(rb, false)
-
-	return hbox
+func newModelHandler() *modelHandler {
+	// You can throw meta data in here
+	m := new(modelHandler)
+	return m
 }
 
-// func makeDataChoosersPage() ui.Control {
-// 	hbox := ui.NewHorizontalBox()
-// 	hbox.SetPadded(true)
+func (mh *modelHandler) ColumnTypes(m *ui.TableModel) []ui.TableValue {
+	return []ui.TableValue{
+		ui.TableString("Job"),		// column 0 text
+		ui.TableString("Location"),		// column 1 text
+		ui.TableString("Company"),		// column 2 text
+	}
+}
 
-// 	vbox := ui.NewVerticalBox()
-// 	vbox.SetPadded(true)
-// 	hbox.Append(vbox, false)
+func (mh *modelHandler) NumRows(m *ui.TableModel) int {
+	return 15
+}
 
-// 	vbox.Append(ui.NewDatePicker(), false)
-// 	vbox.Append(ui.NewTimePicker(), false)
-// 	vbox.Append(ui.NewDateTimePicker(), false)
-// 	vbox.Append(ui.NewFontButton(), false)
-// 	vbox.Append(ui.NewColorButton(), false)
 
-// 	hbox.Append(ui.NewVerticalSeparator(), false)
+func (mh *modelHandler) CellValue(m *ui.TableModel, row, column int) ui.TableValue {
+	fmt.Println(strconv.Itoa(row), strconv.Itoa(column))
+	switch column {
+	case 0:
+		return ui.TableString(fmt.Sprintf("Row %d", row))
+	case 2:
+		return ui.TableString("Editing this won't change anything")
+	case 1:
+		return ui.TableString("Colors!")
+	}
+	panic("At the disco")
+}
 
-// 	vbox = ui.NewVerticalBox()
-// 	vbox.SetPadded(true)
-// 	hbox.Append(vbox, true)
-
-// 	grid := ui.NewGrid()
-// 	grid.SetPadded(true)
-// 	vbox.Append(grid, false)
-
-// 	button := ui.NewButton("Open File")
-// 	entry := ui.NewEntry()
-// 	entry.SetReadOnly(true)
-// 	button.OnClicked(func(*ui.Button) {
-// 		filename := ui.OpenFile(mainwin)
-// 		if filename == "" {
-// 			filename = "(cancelled)"
-// 		}
-// 		entry.SetText(filename)
-// 	})
-// 	grid.Append(button,
-// 		0, 0, 1, 1,
-// 		false, ui.AlignFill, false, ui.AlignFill)
-// 	grid.Append(entry,
-// 		1, 0, 1, 1,
-// 		true, ui.AlignFill, false, ui.AlignFill)
-
-// 	button = ui.NewButton("Save File")
-// 	entry2 := ui.NewEntry()
-// 	entry2.SetReadOnly(true)
-// 	button.OnClicked(func(*ui.Button) {
-// 		filename := ui.SaveFile(mainwin)
-// 		if filename == "" {
-// 			filename = "(cancelled)"
-// 		}
-// 		entry2.SetText(filename)
-// 	})
-// 	grid.Append(button,
-// 		0, 1, 1, 1,
-// 		false, ui.AlignFill, false, ui.AlignFill)
-// 	grid.Append(entry2,
-// 		1, 1, 1, 1,
-// 		true, ui.AlignFill, false, ui.AlignFill)
-
-// 	msggrid := ui.NewGrid()
-// 	msggrid.SetPadded(true)
-// 	grid.Append(msggrid,
-// 		0, 2, 2, 1,
-// 		false, ui.AlignCenter, false, ui.AlignStart)
-
-// 	button = ui.NewButton("Message Box")
-// 	button.OnClicked(func(*ui.Button) {
-// 		ui.MsgBox(mainwin,
-// 			"This is a normal message box.",
-// 			"More detailed information can be shown here.")
-// 	})
-// 	msggrid.Append(button,
-// 		0, 0, 1, 1,
-// 		false, ui.AlignFill, false, ui.AlignFill)
-// 	button = ui.NewButton("Error Box")
-// 	button.OnClicked(func(*ui.Button) {
-// 		ui.MsgBoxError(mainwin,
-// 			"This message box describes an error.",
-// 			"More detailed information can be shown here.")
-// 	})
-// 	msggrid.Append(button,
-// 		1, 0, 1, 1,
-// 		false, ui.AlignFill, false, ui.AlignFill)
-
-// 	return hbox
-// }
+func (mh *modelHandler) SetCellValue(m *ui.TableModel, row, column int, value ui.TableValue) {
+}
 
 func setupUI() {
-	mainwin = ui.NewWindow("libui Control Gallery", 1280, 480, true)
+	mainwin := ui.NewWindow("libui Control Gallery", 640, 480, true)
 	mainwin.OnClosing(func(*ui.Window) bool {
 		ui.Quit()
 		return true
@@ -200,18 +55,23 @@ func setupUI() {
 		return true
 	})
 
-	tab := ui.NewTab()
-	mainwin.SetChild(tab)
+	mh := newModelHandler()
+	model := ui.NewTableModel(mh)
+
+	table := ui.NewTable(&ui.TableParams{
+		Model:	model,
+		RowBackgroundColorModelColumn:	-1,
+	})
+
+	mainwin.SetChild(table)
 	mainwin.SetMargined(true)
 
-	tab.Append("Basic Controls", makeBasicControlsPage())
-	tab.SetMargined(0, true)
-
-	tab.Append("Numbers and Lists", makeNumbersPage())
-	tab.SetMargined(1, true)
-
-	// tab.Append("Data Choosers", makeDataChoosersPage())
-	// tab.SetMargined(2, true)
+	table.AppendTextColumn("Job Title",
+		0, ui.TableModelColumnNeverEditable, nil)
+	table.AppendTextColumn("Company",
+		1, ui.TableModelColumnNeverEditable, nil);
+	table.AppendTextColumn("Location",
+		2, ui.TableModelColumnNeverEditable, nil)
 
 	mainwin.Show()
 }
@@ -219,6 +79,7 @@ func setupUI() {
 func main() {
 	err := ui.Main(setupUI)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 }
+
